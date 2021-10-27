6a2b4b0c27076bc9a1cba2894952d09e4e74d163
Scorcherfjk <franjavikale@gmail.com>
2021-10-27 08:55:46 -0500

    feat: mongodb service

---
+++ app/services/mongoDB.py
@@ -0,0 +1,70 @@
+from uuid import uuid1
+from pymongo import MongoClient
+import os
+
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+
+        if cls not in cls._instances:
+
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class MongoDB(metaclass=SingletonMeta):
+    _connection_string = f"mongodb://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}?ssl=true&replicaSet=atlas-y5g1pb-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+    def __init__(self, collection = 'intents') -> None:
+        self._id = uuid1()
+        self._client = MongoClient(self._connection_string)
+        self._db = self._client[os.environ.get('DB_NAME')]
+        self._collection = self._db[collection]
+        print(f"Connection {self._id} was created")
+
+    def set_collection(self, collection) -> None:
+        self._collection = self._db[collection]
+
+    def read(self, filter, projection = None):
+        return self._collection.find(filter=filter, projection=projection)
+
+    def create(self, payload):
+        doc = self._collection.insert_one(payload)
+        return str(doc.inserted_id)
+
+    def create_many(self, payload):
+        docs = self._collection.insert_many(payload)
+        return docs.inserted_ids
+
+    def update(self, query, newvalues):
+        self._collection.update_one(query, newvalues)
+        return True
+
+    def update_many(self, query, newvalues):
+        docs = self._collection.update_many(query, newvalues)
+        return docs.modified_count
+
+    def delete(self, query):
+        self._collection.delete_one(query)
+        return True
+
+    def delete_many(self, query):
+        docs = self._collection.delete_many(query)
+        return docs.deleted_count
+
+    def close(self):
+        print(f"Connection {self._id} was destroyed")
+        self._client.close()
